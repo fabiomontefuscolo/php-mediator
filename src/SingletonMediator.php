bd4b5b53790ac87fe03fa96ecfe3cdbb81dd6d4b
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2018-05-15 17:41:23 -0300

    Make SingleMediator work on php 7.0, 7.1, 7.2

--- src/SingletonMediator.php
+++ src/SingletonMediator.php
@@ -1,11 +1,12 @@
 <?php
 namespace montefuscolo;
 
-class SingletonMediator extends BaseMediator {
+class SingletonMediator  {
     private static $instance = null;
+    private $mediator = null;
 
     private function __construct() {
-        parent::__construct();
+        $this->mediator = new BaseMediator();
     }
 
     public static function getInstance() {
@@ -13,5 +14,29 @@ class SingletonMediator extends BaseMediator {
             self::$instance = new self();
         }
         return self::$instance;
+    }
+
+    public function add_filter($name, $callback, $priority=10) {
+        $this->mediator->add_filter($name, $callback, $priority);
+    }
+
+    public function add_action($name, $callback, $priority=10) {
+        $this->mediator->add_action($name, $callback, $priority);
+    }
+
+    public function remove_filter($name, $func=null) {
+        return $this->mediator->remove_filter($name, $func);
+    }
+
+    public function remove_action($name, $func=null) {
+        return $this->mediator->remove_action($name, $func);
+    }
+
+    public function run_filters($name, $subject=null) {
+        return $this->mediator->run_filters($name, $subject);
+    }
+
+    public function run_actions($name, $subject=null) {
+        return $this->mediator->run_actions($name, $subject);
     }
 }
